54ba06ca36cb227d205c5a8ca61fce7f197954bf
Lucas <lguerreiroms@gmail.com>
2025-05-29 18:24:05 -0300

    Criando a pagina Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Home from "./Routes/Home";
 import Login from "./Routes/Login";
 import Cadastrar from "./Routes/Cadastrar";
 import Error from "./Routes/Error";
+import Sobre from "./Routes/Sobre";
 
 
 function App() {
   return (
     <>
-      <Error/>
+      <Sobre/>
     </>
   )
 }
