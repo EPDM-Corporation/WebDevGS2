8f257b67f2765bde926adc44bfea34ebd60ec11d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-06-02 17:52:20 -0300

    feat:Estilizando elementos Home Sobre e Solução do Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <header className='h-[118px] bg-[#1A2F08] flex justify-around items-center'>
-        <img src={Logo} alt="" className='w-[140px]'/>
+        <Link to="/"><img src={Logo} alt="" className='w-[140px]'/></Link>
         {/* NAV */}
         <div className='flex justify-around items-center gap-7'>
-            <Link to="/">Home</Link>
-            <Link to="/sobre">Sobre</Link>
-            <Link to="/">Solução</Link>
+            <Link to="/" className='text-[18px] text-[#FFFFFF] transition duration-300 ease-in-out hover:text-gray-500'>Home</Link>
+            <Link to="/sobre" className='text-[18px] text-[#FFFFFF] transition duration-300 ease-in-out hover:text-gray-500'>Sobre</Link>
+            <Link to="/" className='text-[18px] text-[#FFFFFF] transition duration-300 ease-in-out hover:text-gray-500'>Solução</Link>
         </div>
     </header>
   )
