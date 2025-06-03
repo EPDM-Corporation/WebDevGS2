080f0a4e3f9a97d7c295c477bb31f86e0c4ce0f1
Lucas <lguerreiroms@gmail.com>
2025-06-03 12:44:35 -0300

    Criando a pagina solucao

--- src/Routes/Solucao.jsx
+++ src/Routes/Solucao.jsx
@@ -9,7 +9,7 @@ const Solucao = () =>{
                 <h1 className='text-6xl mb-10'>Solução</h1>
                 <h2 className='text-lg'>Nossa solução para o monitoramento de inundações é baseada no uso de tecnologia acessível e eficiente.<br/> Propomos a utilização do Arduino ESP32 integrado com sensores de nível de água, que serão instalados <br/>nas margens dos rios. Esses sensores realizam medições constantes da altura do rio e enviam os dados <br/>automaticamente por meio de rede sem fio.
 
-                            As informações coletadas são processadas e exibidas em um mapa<br/> virtual interativo, que utiliza um sistema de cores para indicar o nível de risco: verde para nível normal<br/> sem risco, amarelo para situação de atenção com risco moderado, e vermelho para alerta máximo de inundação.<br/>
+                            As informações coletadas são processadas e exibidas em um mapa<br/> virtual, que utiliza um sistema de cores para indicar o nível de risco: verde para nível normal<br/> sem risco, amarelo para situação de atenção com risco moderado, e vermelho para alerta máximo de inundação.<br/>
 
                             Com esse sistema, é possível acompanhar remotamente a situação de diversos pontos de medição, permitindo<br/> que autoridades e comunidades tomem decisões rápidas e precisas para evitar desastres.<br/>  Nosso objetivoé oferecer uma ferramenta de baixo custo, de fácil instalação e alta eficiência, <br/>que contribua diretamente para a prevenção de enchentes e para a proteção<br/> das populações ribeirinhas.</h2>
             </div>
