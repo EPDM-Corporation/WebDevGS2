5662cd2d15c61d181c7052ef649171d9285b46a5
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-06-05 22:19:38 -0300

    refactor(login/cadastro):Removendo o componente e colocando o código separadamente

--- src/Routes/Cadastrar.jsx
+++ src/Routes/Cadastrar.jsx
@@ -1,21 +1,28 @@
-import EmailSenha from "../Components/EmailSenha";
-import CadastrarImagem from "..//assets/Cadastrar.PNG";
+import CadastrarImagem from "../assets/Cadastrar.PNG";
 import Voltar from "../Components/Voltar";
 
-const Cadastrar = () =>{
+const Cadastrar = () => {
     return(
         <>
         <div className="flex relative">
             <Voltar/>
-            <EmailSenha
-            title="Cadastrar"
-            subtitle="Cadastre seu Email e Senha "
-            buttonText="Cadastrar"
-            />
+            <div className="bg-[#24140B] w-3/5 h-screen text-[#CDC8C8] flex flex-col items-center justify-center">
+                <div className="pb-10 w-1/2">
+                    <h1 className="text-4xl font-bold">Cadastrar</h1>
+                    <h2 className="text-xl pt-3">Cadastre seu Email e Senha</h2>
+                </div>
+                <form className="w-1/2">
+                    <h2>Endereço de Email</h2>
+                    <input type="email" placeholder="Digite Aqui Seu Email" className="border-2 border-[#CDC8C8] rounded-lg mb-5 w-full pl-3 mt-2"></input>
+                    <h2>Senha</h2>
+                    <input type="password" placeholder="Digite Sua Senha Aqui" className="border-2 border-[#CDC8C8] rounded-lg mb-10 w-full pl-3 mt-2"></input>
+                    <button type="submit" className="bg-[#946631] my-2 rounded-xl font-bold justify-center w-1/2 flex p-2 hover:scale-95 hover:text-white">Cadastrar</button>
+                </form>
+            </div>
             <img src={CadastrarImagem} className="w-2/5 h-screen object-cover"/>
         </div>
-            
         </>
     )
 }
+
 export default Cadastrar;
--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -1,20 +1,33 @@
-import EmailSenha from "../Components/EmailSenha";
 import Voltar from "../Components/Voltar";
 import LoginImage from "../assets/Login.PNG"; // Assuming you have a login image
 import { Link } from "react-router-dom";
-const Login = () =>{
+
+const Login = () => {
     return(
         <section className="flex">
             <Voltar/>
-            <EmailSenha 
-            title="Login"
-            subtitle="Entre com seu Email e Senha"
-            buttonText="Login"
-            type="login"/>
+            <div className="bg-[#24140B] w-3/5 h-screen text-[#CDC8C8] flex flex-col items-center justify-center">
+                <div className="pb-10 w-1/2">
+                    <h1 className="text-4xl font-bold">Login</h1>
+                    <h2 className="text-xl pt-3">Entre com seu Email e Senha</h2>
+                </div>
+                <form className="w-1/2">
+                    <h2>Endereço de Email</h2>
+                    <input type="email" placeholder="Digite Aqui Seu Email" className="border-2 border-[#CDC8C8] rounded-lg mb-5 w-full pl-3 mt-2"></input>
+                    <h2>Senha</h2>
+                    <input type="password" placeholder="Digite Sua Senha Aqui" className="border-2 border-[#CDC8C8] rounded-lg mb-10 w-full pl-3 mt-2"></input>
+                    <button type="submit" className="bg-[#946631] my-2 rounded-xl font-bold justify-center w-1/2 flex p-2 hover:scale-95 hover:text-white">Login</button>
+                    <Link to="/cadastrar">
+                        <button className="bg-[#946631] my-2 rounded-xl font-bold justify-center w-1/2 flex p-2 hover:scale-95 hover:text-white">
+                            Quero cadastrar
+                        </button>
+                    </Link>
+                </form>
+            </div>
 
             <img src={LoginImage} alt="Login" className="w-2/5 h-screen object-cover"/>
         </section>
-
     )
 }
+
 export default Login;
